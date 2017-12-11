66bda4c8306b3e4ef72e11c1af166cbeda551d7e
savikdf <savikdf@hotmail.com>
2017-12-10 21:04:57 -1000

    Added : Scene and Manager
    Facebook SDK and gameMan Setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Facebook;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : MonoBehaviour
+{
+    protected enum GamesStates
+    {
+        Init,
+        Load,
+        Game,
+        Post,
+        Ad,
+        Purchase
+    }
+    bool setupErroredOut;
 
     private void Awake()
     {
+        //Instantiate all sub managers
+        setupErroredOut = DeploySubManagers();
 
     }
 
+    bool DeploySubManagers()
+    {
+        bool erroredOut = false;
+        try
+        {
+            
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+            erroredOut = true;
+        }          
+        return erroredOut;
+    }
 
 }
