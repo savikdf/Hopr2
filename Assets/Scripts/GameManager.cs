6f4819fe23c1802d3aa490450cca1785912ba182
savikdf <savikdf@hotmail.com>
2017-12-10 19:55:07 -1000

    Added : Scene and Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook;
+
+public class GameManager : MonoBehaviour {
+
+
+    private void Awake()
+    {
+
+    }
+
+
+}
