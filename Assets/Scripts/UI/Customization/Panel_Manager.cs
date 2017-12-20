21e1853de34489246f5e3681ab43527ab314733c
Togene <jhonnywhiplash@hotmail.com.au>
2017-12-21 04:21:54 +1000

    Added : UI Panels, New Corners, Updated Panel Manager

--- Assets/Scripts/UI/Customization/Panel_Manager.cs
+++ Assets/Scripts/UI/Customization/Panel_Manager.cs
@@ -9,17 +9,22 @@ public class Panel_Manager : MonoBehaviour {
     public string text;
     public Color PanelColor;
     public Color PanelShadowColor;
+    public Color PanelOutlineColor;
+    public Color textColor;
+    public bool showCorners;
 
 
     GameObject panelBits;
     GameObject panelBitsShadow;
     GameObject panelText;
+    GameObject panelOutline;
 
 	// Use this for initialization
 	void Start () {
         panelBits = this.gameObject.transform.GetChild(1).gameObject;
         panelBitsShadow = this.gameObject.transform.GetChild(0).gameObject;
-        panelText = this.gameObject.transform.GetChild(2).gameObject;
+        panelOutline = this.gameObject.transform.GetChild(2).gameObject;
+        panelText = this.gameObject.transform.GetChild(3).gameObject;
     }
 	
 	// Update is called once per frame
@@ -35,13 +40,30 @@ public class Panel_Manager : MonoBehaviour {
             {
                 img.color = PanelColor;
             }
+        }
 
+        if (panelBitsShadow != null)
+        {
             foreach (Image img in panelBitsShadow.GetComponentsInChildren<Image>())
             {
                 img.color = PanelShadowColor;
             }
-
-            panelText.GetComponent<Text>().text = text;
         }
+
+        if (panelOutline != null)
+        {
+            foreach (Image img in panelOutline.GetComponentsInChildren<Image>())
+            {
+                img.color = PanelOutlineColor;
+            }
+        }
+
+
+        if (panelText != null) { panelText.GetComponent<Text>().text = text; panelText.GetComponent<Text>().color = textColor; }
+
+        if (panelOutline != null) panelOutline.gameObject.SetActive(showCorners);
+        
+
+
     }
 }
