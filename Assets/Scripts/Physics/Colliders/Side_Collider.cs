4854c2767c58f3aabd60d97835ef5da90f767908
Togene <jhonnywhiplash@hotmail.com.au>
2018-02-16 17:30:39 +1000

    Finalizing : Side Colliders,
    Creating 3 smaller traingles to act as a sort of primitive mesh collider

--- Assets/Scripts/Physics/Colliders/Side_Collider.cs
+++ Assets/Scripts/Physics/Colliders/Side_Collider.cs
@@ -6,23 +6,35 @@ public class Side_Collider : MonoBehaviour
 {
 
     public Face[] face;
+    public Vector3[] vertices;
     // Use this for initialization
     void Start()
     {
 
-     face = new Face[3];
+     face = new Face[4];
 
      Vector3 c = transform.localPosition;
       //
-     Vector3[] vertices = new Vector3[this.GetComponent<MeshFilter>().mesh.vertices.Length];
+     vertices = new Vector3[this.GetComponent<MeshFilter>().mesh.vertices.Length];
      vertices = this.GetComponent<MeshFilter>().mesh.vertices;
 //   
      Vector3 l = ((new Vector3(vertices[11].x, vertices[11].y, 0)));
      Vector3 r = ((new Vector3(vertices[1].x, vertices[1].y, 0)));
 //   
-     face[0] = new Face(l, r, c, Vector3.up, false, 1.10f, this.gameObject);
-     face[1] = new Face(r, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.10f, this.gameObject);
-     face[2] = new Face(l, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.10f, this.gameObject);
+
+     Vector3 innerl = ((new Vector3(vertices[3].x, vertices[3].y, 0)));
+     Vector3 innerr = ((new Vector3(vertices[10].x, vertices[10].y, 0)));
+
+     //Vertices index 3 and 10
+
+     // Big Triangle
+     face[0] = new Face(l, r, c, Vector3.up, false, 1.0f, this.gameObject);
+     face[1] = new Face(r, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
+     face[2] = new Face(l, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
+
+     //centre front traingle
+
+     face[3] = new Face(innerl, innerr, c, Vector3.up, false, 1.0f, this.gameObject);
     }
 
     // Update is called once per frame
@@ -37,11 +49,15 @@ public class Side_Collider : MonoBehaviour
     {
         if (Application.isPlaying)
         {
+            foreach(Vector3 vert in vertices)
+                Gizmos.DrawSphere(transform.TransformPoint(vert), .01f);
+            
             if (VariableManager.P_Options.showDebugs)
             {
                 face[0].DrawFace();
                 face[1].DrawFace();
                 face[2].DrawFace();
+                face[3].DrawFace();
             }
         }
     }
