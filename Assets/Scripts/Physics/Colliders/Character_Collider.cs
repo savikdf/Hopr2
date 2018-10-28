10fcd5d3c5651a50721d7abfc2d12fb2548b5e9e
Togene <genejmartens@gmail.com>
2018-10-29 04:21:10 +1000

    Fixed Meta Issue
    Meta Data isnt being pushed for some reason, gana have to sort that out

--- Assets/Scripts/Physics/Colliders/Character_Collider.cs
+++ Assets/Scripts/Physics/Colliders/Character_Collider.cs
@@ -38,41 +38,41 @@ public class Character_Collider : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-     //  //Front Face
-     //  faces[0] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(-2, 15, 1f), new Vector3(2, 15, 1f),
-     //  new Vector3(-2, 0, 1f), new Vector3(2, 0, 1f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
+       //Front Face
+       faces[0] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(-2, 15, 1f), new Vector3(2, 15, 1f),
+       new Vector3(-2, 0, 1f), new Vector3(2, 0, 1f),
+       new Vector3(0, 1, 0), transform.gameObject);
 //
-     //  //Back Face
-     //  faces[1] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(-2, 15, -2f), new Vector3(2, 15, -2f),
-     //  new Vector3(-2, 0, -2f), new Vector3(2, 0, -2f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
+       //Back Face
+       faces[1] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(-2, 15, -2f), new Vector3(2, 15, -2f),
+       new Vector3(-2, 0, -2f), new Vector3(2, 0, -2f),
+       new Vector3(0, 1, 0), transform.gameObject);
 
-     //  //Top Face
-     //  faces[2] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(-2, 15, 1f), new Vector3(2, 15, 1f),
-     //  new Vector3(-2, 15,  -2f), new Vector3(2, 15, -2f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
+       //Top Face
+       faces[2] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(-2, 15, 1f), new Vector3(2, 15, 1f),
+       new Vector3(-2, 15,  -2f), new Vector3(2, 15, -2f),
+       new Vector3(0, 1, 0), transform.gameObject);
 //
-     //  //Top Face
-     //  faces[3] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(-2, 0, 1f), new Vector3(2, 0, 1f),
-     //  new Vector3(-2, 0,  -2f), new Vector3(2, 0, -2f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
-     //  
-     //  //Right Face
-     //  faces[4] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(-2, 15, 1f), new Vector3(-2, 15, -2f),
-     //  new Vector3(-2, 0, 1f), new Vector3(-2, 0, -2f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
+       //Top Face
+       faces[3] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(-2, 0, 1f), new Vector3(2, 0, 1f),
+       new Vector3(-2, 0,  -2f), new Vector3(2, 0, -2f),
+       new Vector3(0, 1, 0), transform.gameObject);
+       
+       //Right Face
+       faces[4] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(-2, 15, 1f), new Vector3(-2, 15, -2f),
+       new Vector3(-2, 0, 1f), new Vector3(-2, 0, -2f),
+       new Vector3(0, 1, 0), transform.gameObject);
 
-     //  //Left Face
-     //  faces[5] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
-     //  new Vector3(2, 15, 1f), new Vector3(2, 15, -2f),
-     //  new Vector3(2, 0, 1f), new Vector3(2, 0, -2f),
-     //  new Vector3(0, 1, 0), transform.gameObject);
+       //Left Face
+       faces[5] = new Face(transform.parent.position + new Vector3(0, 0f, 0),
+       new Vector3(2, 15, 1f), new Vector3(2, 15, -2f),
+       new Vector3(2, 0, 1f), new Vector3(2, 0, -2f),
+       new Vector3(0, 1, 0), transform.gameObject);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Physics/Colliders/Side_Collider.cs
+++ Assets/Scripts/Physics/Colliders/Side_Collider.cs
@@ -8,47 +8,47 @@ public class Side_Collider : MonoBehaviour
     public Segment[] segment;
     //public Vector3[] vertices;
     // Use this for initialization
-    public Vector3 testPoint;
+    //public Vector3 testPoint;
     void Start()
     {
 
-       //  segment = new Segment[12];
+     // segment = new Segment[12];
 //
-       //  Vector3 c = transform.localPosition;
-       //  //
-       //  vertices = new Vector3[this.GetComponent<MeshFilter>().mesh.vertices.Length];
-       //  vertices = this.GetComponent<MeshFilter>().mesh.vertices;
-       //  //   
-       //  Vector3 l = ((new Vector3(vertices[11].x, vertices[11].y, 0)));
-       //  Vector3 r = ((new Vector3(vertices[1].x, vertices[1].y, 0)));
-       //  //   
+     // Vector3 c = transform.localPosition;
+     // //
+     // vertices = new Vector3[this.GetComponent<MeshFilter>().mesh.vertices.Length];
+     // vertices = this.GetComponent<MeshFilter>().mesh.vertices;
+     // //   
+     // Vector3 l = ((new Vector3(vertices[11].x, vertices[11].y, 0)));
+     // Vector3 r = ((new Vector3(vertices[1].x, vertices[1].y, 0)));
+     // //   
 //
-       //  Vector3 innerr = ((new Vector3(vertices[3].x, vertices[3].y, 0)));
-       //  Vector3 innerl = ((new Vector3(vertices[10].x, vertices[10].y, 0)));
+     // Vector3 innerr = ((new Vector3(vertices[3].x, vertices[3].y, 0)));
+     // Vector3 innerl = ((new Vector3(vertices[10].x, vertices[10].y, 0)));
 //
-       //  Vector3 innerc = Utils.MidPoint(l, r);
-       //  //Vertices index 3 and 10
+     // Vector3 innerc = Utils.MidPoint(l, r);
+     // //Vertices index 3 and 10
 //
-       //  // Big Triangle
-       //  segment[0] = new Segment(l, r, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[1] = new Segment(r, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[2] = new Segment(l, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
+     // // Big Triangle
+     // segment[0] = new Segment(l, r, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[1] = new Segment(r, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[2] = new Segment(l, transform.InverseTransformPoint(transform.parent.position), c, Vector3.up, false, 1.0f, this.gameObject);
 //
-       //  //front traingle
-       //  segment[3] = new Segment(innerl, innerr, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[4] = new Segment(innerr, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[5] = new Segment(innerl, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // //front traingle
+     // segment[3] = new Segment(innerl, innerr, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[4] = new Segment(innerr, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[5] = new Segment(innerl, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
 //
-       //  //front right traingle
-       //  segment[6] = new Segment(innerl, l, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[7] = new Segment(innerl, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[8] = new Segment(l, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // //front right traingle
+     // segment[6] = new Segment(innerl, l, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[7] = new Segment(innerl, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[8] = new Segment(l, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
 //
 //
-       //  //front left traingle
-       //  segment[9] = new Segment(innerr, r, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[10] = new Segment(innerr, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
-       //  segment[11] = new Segment(r, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // //front left traingle
+     // segment[9] = new Segment(innerr, r, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[10] = new Segment(innerr, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
+     // segment[11] = new Segment(r, innerc, c, Vector3.up, false, 1.0f, this.gameObject);
     }
 
     // Update is called once per frame
